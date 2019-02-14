2f996561150c57df5999d5f423e385713a2076d1
sundashuai123 <2325354298@qq.com>
2019-02-14 16:46:05 +0800

    登录路由配置完毕

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
-
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/components/login.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [{
-    path: '/',
-    component: HelloWorld
+    path: '/'
+  }, {
+    name: 'login',
+    path: '/login',
+    component: Login
   }]
 })
