1f91e2840c89adeb5f98bf523c0b3edac65db828
sundashuai123 <2325354298@qq.com>
2019-02-15 21:37:03 +0800

    获取首页数据

--- src/components/users.vue
+++ src/components/users.vue
@@ -21,6 +21,27 @@
         <el-button type="success" @click="showDiaAddUser()">添加用户</el-button>
       </el-col>
     </el-row>
+    <el-table height="350px" :data="list" style="width: 100%">
+      <el-table-column prop="id" label="#" width="80"></el-table-column>
+      <el-table-column prop="username" label="姓名" width="120"></el-table-column>
+      <el-table-column prop="email" label="邮箱" width="140"></el-table-column>
+      <el-table-column prop="mobile" label="电话" width="140"></el-table-column>
+      <el-table-column label="创建日期" width="140">
+        <template slot-scope="scope">{{scope.row.create_time | fmtdate}}</template>
+      </el-table-column>
+      <el-table-column label="用户状态" width="140">
+        <template slot-scope="scope">
+          <el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" width="200">
+        <template slot-scope="scope">
+          <el-button type="primary" icon="el-icon-edit" circle size="mini" plain></el-button>
+          <el-button type="danger" icon="el-icon-delete" circle size="mini" plain></el-button>
+          <el-button type="success" icon="el-icon-check" circle size="mini" plain></el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </el-card>
 </template>
 
@@ -28,7 +49,25 @@
 export default {
   data () {
     return {
-      query: ''
+      query: '',
+      pagenum: 1,
+      pagesize: 2,
+      list: []
+    }
+  },
+  created () {
+    this.getTableData()
+  },
+  methods: {
+    async getTableData () {
+      const AUTH_TOKEN = localStorage.getItem('token')
+      this.$http.defaults.headers.common['Authorization'] = AUTH_TOKEN
+      const res = await this.$http.get(
+        `users?query=${this.query}&pagenum=${this.pagenum}&pagesize=${
+          this.pagesize
+        }`
+      )
+      console.log(res)
     }
   }
 }
