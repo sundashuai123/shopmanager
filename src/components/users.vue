2432296b8387144804b173fd3b81979767effb54
sundashuai123 <2325354298@qq.com>
2019-02-15 21:07:02 +0800

    搜索-添加-面包屑

---
+++ src/components/users.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-card class="box">
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-row class="seartBox">
+      <el-col>
+        <!-- 搜索框 -->
+        <el-input
+          @clear="getAllUsers()"
+          clearable
+          class="searchInput"
+          placeholder="请输入内容"
+          v-model="query"
+        >
+          <el-button slot="append" icon="el-icon-search" @click="searchUser()"></el-button>
+        </el-input>
+        <!-- 添加按钮 -->
+        <el-button type="success" @click="showDiaAddUser()">添加用户</el-button>
+      </el-col>
+    </el-row>
+  </el-card>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      query: ''
+    }
+  }
+}
+</script>
+
+<style>
+.box {
+  height: 100%;
+}
+.seartBox {
+  margin-top: 20px;
+}
+.searchInput {
+  width: 350px;
+}
+.page {
+  margin-top: 20px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/login.vue'
 import Home from '@/components/home.vue'
+import Users from '@/components/users.vue'
 
 Vue.use(Router)
 
@@ -9,7 +10,12 @@ export default new Router({
   routes: [{
     path: '/',
     name: 'home',
-    component: Home
+    component: Home,
+    children: [{
+      path: '/users',
+      name: 'users',
+      component: Users
+    }]
   }, {
     name: 'login',
     path: '/login',
